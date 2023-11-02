8ffb44d043ab57cd67cb23b90f46d1481a70ea93
carinasylver <carinasylver@pop-os.localdomain>
2023-11-02 17:51:56 -0300

    exercicio 06 permissão

--- exercicios_views_e_permissoes/06_permissao.sql
+++ exercicios_views_e_permissoes/06_permissao.sql
@@ -6,9 +6,12 @@ GRANT SELECT, UPDATE, INSERT, DELETE
 ON itens 
 TO time_de_vendas;
 
-UPDATE itens
-SET valor = valor * 1.1
-WHERE codigo IN (6, 7, 8);
+UPDATE 
+  itens
+SET 
+  valor = valor * 1.1
+WHERE 
+  codigo IN (6, 7, 8);
 
 
 SELECT 
