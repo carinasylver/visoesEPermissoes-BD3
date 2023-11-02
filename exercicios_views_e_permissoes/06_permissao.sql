92b03de18ed4bf80cad73fd6a94f85c9b3b97935
carinasylver <carinasylver@pop-os.localdomain>
2023-11-02 17:39:24 -0300

    exercicio 06 permissão

---
+++ exercicios_views_e_permissoes/06_permissao.sql
@@ -0,0 +1,11 @@
+CREATE GROUP time_de_vendas;
+
+ALTER GROUP time_de_vendas ADD USER cleber, miguel ;
+
+GRANT SELECT, UPDATE, INSERT, DELETE
+ON itens 
+TO time_de_vendas;
+
+UPDATE itens
+SET valor = valor * 1.1
+WHERE codigo IN (6, 7, 8);
